2c87f5dfd90735787850bd043c8e3ae138287d31
datouboy <datouboy@vip.qq.com>
2017-02-17 12:03:34 +0800

    完善裁图功能，改写成prototype方法
    完善裁图功能，改写成prototype方法

--- dest/js/app.js
+++ dest/js/app.js
@@ -1,176 +1,252 @@
-$('#cuttingImgBox').width($('#cuttingImgBox').width());
-$('#cuttingImgBox').height($('#cuttingImgBox').height());
-$('#oldImage').width($('#oldImage').width());
-$('#oldImage').height($('#oldImage').height());
-
-var myCuttingImgBox = document.getElementById('oldImage');
-var mc = new Hammer(myCuttingImgBox);
-mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
-mc.get('pinch').set({ enable: true });
-
-var oldImageInfo = {
-	left : 0,
-	top : 0,
-	//位移动作touchstart时的left和top值
-	tempLeft : 0,
-	tempTop : 0,
-	//图片原始宽/高
-	width : $('#oldImage').width(),
-	height : $('#oldImage').height(),
-	//变化后图片宽/高
-	now_width : $('#oldImage').width(),
-	now_height : $('#oldImage').height(),
-	//获取图片的宽高比
-	imgAspectRatio : $('#oldImage').width() / $('#oldImage').height(),
-	//获取截图框的宽高比
-	boxAspectRatio : $('#cuttingImgBox').width() / $('#cuttingImgBox').height(),
-	//touchend时间戳
-	touchendTime : 0,
-	//缩放完成时间戳
-	pinchendTime : 0,
-	//图片缩放状态（配合双击事件）
-	zoomState : true
-}
-
-myCuttingImgBox.addEventListener('touchstart',touch,false);
-myCuttingImgBox.addEventListener('touchend',touch,false);
-
-function touch (event){
-    var event = event || window.event;
-    switch(event.type){
-        case "touchstart":
-            oldImageInfo.tempLeft = $('#oldImage').position().left;
-            oldImageInfo.tempTop = $('#oldImage').position().top;
-            break;
-        case "touchend":
-        	var touchendTime = new Date().getTime();
-        	if(touchendTime - oldImageInfo.pinchendTime > 300){
-        	//判断双击时间间隔
-	        	if(touchendTime - oldImageInfo.touchendTime < 300){
-	        		if(oldImageInfo.zoomState){
-		        		$('#oldImage').css({
-					    	width: $('#cuttingImgBox').height() * oldImageInfo.imgAspectRatio,
-					    	height : $('#cuttingImgBox').height(),
-					    	left: ($('#cuttingImgBox').width() - ($('#cuttingImgBox').height() * oldImageInfo.imgAspectRatio))/2,
-					    	top: 0
-					    });
-					    oldImageInfo.zoomState = false;
-		        	}else{
-		        		$('#oldImage').css({
-					    	width: oldImageInfo.width,
-					    	height : oldImageInfo.height,
-					    	left: 0,
-					    	top: 0
-					    });
-					    oldImageInfo.zoomState = true;
-		        	}
-		        	setTimeout(function(){
-					    oldImageInfo.now_width = $('#oldImage').width();
-						oldImageInfo.now_height = $('#oldImage').height();
-						oldImageInfo.left = $('#oldImage').position().left;
-					    oldImageInfo.top = $('#oldImage').position().top;
-				    },20);
-	        	}
-        	}
-        	oldImageInfo.touchendTime = new Date().getTime();
-            break;
-    }
-}
-
-mc.on('pan', function(ev) {
-    //console.log(ev);
-    if(new Date().getTime() - oldImageInfo.pinchendTime > 100){
-	    $('#oldImage').css({
-	    	left: oldImageInfo.left + ev.deltaX,
-	    	top : oldImageInfo.top + ev.deltaY
-	    });
-	    if(ev.isFinal){
-	    	
-	    	checkError();
-			setTimeout(function(){
-				oldImageInfo.left = $('#oldImage').position().left;
-	    		oldImageInfo.top = $('#oldImage').position().top;
-			},230);
+function AlexCuttingImg(obj){this.init(obj);}
+AlexCuttingImg.prototype = {
+    init:function(obj){
+    	var _self = this;
+    	this.cuttingImgBoxID = obj.cuttingImgBoxID;
+    	this.imgID = obj.imgID;
+    	this.myCuttingImgBox = document.getElementById(obj.imgID);
+    	this.imageInfo = {
+			left : 0,
+			top : 0,
+			//位移动作touchstart时的left和top值
+			tempLeft : 0,
+			tempTop : 0,
+			//图片原始宽/高
+			width : $('#'+this.imgID).width(),
+			height : $('#'+this.imgID).height(),
+			//变化后图片宽/高
+			now_width : $('#'+this.imgID).width(),
+			now_height : $('#'+this.imgID).height(),
+			//获取图片的宽高比
+			imgAspectRatio : $('#'+this.imgID).width() / $('#'+this.imgID).height(),
+			//获取截图框的宽高比
+			boxAspectRatio : $('#'+this.cuttingImgBoxID).width() / $('#'+this.cuttingImgBoxID).height(),
+			//touchend时间戳
+			touchendTime : 0,
+			//缩放完成时间戳
+			pinchendTime : 0,
+			//图片缩放状态（配合双击事件）
+			zoomState : true
 		}
-		//$('#showInfo').append('pan ');
-	}
-});
+		//初始化裁剪框大小及图片大小
+		this.setInit();
 
-mc.on('pinch', function(ev) {
-	//console.log(ev);
-	//$('#showInfo').html('调试信息：' + ($('#oldImage').position().left + ((oldImageInfo.now_width - $('#oldImage').width())/2)));
-	var scale = Math.round(ev.scale*100)/100;
-	$('#oldImage').css({
-    	width: oldImageInfo.now_width * scale,
-    	height : oldImageInfo.now_height * scale,
-    	left: oldImageInfo.tempLeft + ((oldImageInfo.now_width - $('#oldImage').width())/2),
-    	top: oldImageInfo.tempTop + ((oldImageInfo.now_height - $('#oldImage').height())/2)
-    });
-});
+    	//初始化配置Hammer
+    	var mc = new Hammer(this.myCuttingImgBox);
+    	mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+		mc.get('pinch').set({ enable: true });
 
-mc.on('pinchend', function(ev) {
-	//console.log(ev);
-	//$('#showInfo').html('调试信息：' + ($('#oldImage').position().left + ((oldImageInfo.now_width - $('#oldImage').width())/2)));
+		//添加监听事件
+		this.myCuttingImgBox.addEventListener('touchstart',function(e,some){ _self.touch(e,_self); },false);
+		this.myCuttingImgBox.addEventListener('touchend',function(e,some){ _self.touch(e,_self); },false);
 
-	//检测图片最小宽高
-	if($('#oldImage').width() < $('#cuttingImgBox').width() && $('#oldImage').height() < $('#cuttingImgBox').height()){
-    	//图片高度大于截图框
-    	if(oldImageInfo.imgAspectRatio < oldImageInfo.boxAspectRatio){
-    		//以图片最小高度为准
-    		//$('#showInfo').html('调试信息：' + $('#cuttingImgBox').width() + ' --- ' + ($('#cuttingImgBox').width() - ($('#cuttingImgBox').width() * oldImageInfo.imgAspectRatio))/2);
+		mc.on('pan', function(ev) {
+		    if(new Date().getTime() - _self.imageInfo.pinchendTime > 100){
+		    	if(ev.center.y > 0){
+				    $('#'+_self.imgID).css({
+				    	left: _self.imageInfo.left + ev.deltaX,
+				    	top : _self.imageInfo.top + ev.deltaY
+				    });
+				    if(ev.isFinal){
+				    	_self.checkError();
+						setTimeout(function(){
+							_self.imageInfo.left = $('#'+_self.imgID).position().left;
+				    		_self.imageInfo.top = $('#'+_self.imgID).position().top;
+						},130);
+					}
+				}else{
+					_self.checkError();
+					setTimeout(function(){
+						_self.imageInfo.left = $('#'+_self.imgID).position().left;
+			    		_self.imageInfo.top = $('#'+_self.imgID).position().top;
+					},130);
+				}
+			}
+		});
 
-			$('#oldImage').css({
-		    	width: $('#cuttingImgBox').height() * oldImageInfo.imgAspectRatio,
-		    	height : $('#cuttingImgBox').height(),
-		    	left: ($('#cuttingImgBox').width() - ($('#cuttingImgBox').height() * oldImageInfo.imgAspectRatio))/2,
-		    	top: 0
+		mc.on('pinch', function(ev) {
+			var scale = Math.round(ev.scale*100)/100;
+			$('#'+_self.imgID).css({
+		    	width: _self.imageInfo.now_width * scale,
+		    	height : _self.imageInfo.now_height * scale,
+		    	left: _self.imageInfo.tempLeft + ((_self.imageInfo.now_width - $('#'+_self.imgID).width())/2),
+		    	top: _self.imageInfo.tempTop + ((_self.imageInfo.now_height - $('#'+_self.imgID).height())/2)
 		    });
-    	}else{//图片高度小于截图框
-    		//以图片最小宽度为准
-    		$('#oldImage').css({
-		    	width: oldImageInfo.width,
-		    	height : oldImageInfo.height,
-		    	left: 0,
-		    	top: 0
-		    });
-    	}
-	}
+		});
 
-	checkError();
-	setTimeout(function(){
-	    oldImageInfo.now_width = $('#oldImage').width();
-		oldImageInfo.now_height = $('#oldImage').height();
-		oldImageInfo.left = $('#oldImage').position().left;
-	    oldImageInfo.top = $('#oldImage').position().top;
-    },230);
-    oldImageInfo.pinchendTime = new Date().getTime();
-	//$('#showInfo').append('pinchend ');
-});
+		mc.on('pinchend', function(ev) {
+			//检测图片最小宽高
+			if($('#'+_self.imgID).width() < $('#'+_self.cuttingImgBoxID).width() && $('#'+_self.imgID).height() < $('#'+_self.cuttingImgBoxID).height()){
+		    	//图片高度大于截图框
+		    	if(_self.imageInfo.imgAspectRatio < _self.imageInfo.boxAspectRatio){
+		    		//以图片最小高度为准
+					$('#'+_self.imgID).css({
+				    	width: $('#'+_self.cuttingImgBoxID).height() * _self.imageInfo.imgAspectRatio,
+				    	height : $('#'+_self.cuttingImgBoxID).height(),
+				    	left: ($('#'+_self.cuttingImgBoxID).width() - ($('#'+_self.cuttingImgBoxID).height() * _self.imageInfo.imgAspectRatio))/2,
+				    	top: 0
+				    });
+		    	}else{//图片高度小于截图框
+		    		//以图片最小宽度为准
+		    		$('#'+_self.imgID).css({
+				    	width: _self.imageInfo.width,
+				    	height : _self.imageInfo.height,
+				    	left: 0,
+				    	top: 0
+				    });
+		    	}
+			}
 
-function checkError(){
-	//检测图片移动边界值
-	//右边界
-	if($('#oldImage').position().left >= $('#cuttingImgBox').width()-10){
-		$('#oldImage').animate({
-	    	left: ($('#cuttingImgBox').width() - ($('#cuttingImgBox').width()/3)) + 'px'
-	    },100);
-	}
-	//下边界
-	if($('#oldImage').position().top >= $('#cuttingImgBox').height()-10){
-		$('#oldImage').animate({
-	    	top: ($('#cuttingImgBox').height() - ($('#cuttingImgBox').height()/3)) + 'px'
-	    },100);
-	}
-	//左边界
-	if($('#oldImage').width() + $('#oldImage').position().left <= 10){
-		$('#oldImage').animate({
-	    	left: (-$('#oldImage').width() + ($('#cuttingImgBox').width()/3)) + 'px'
-	    },100);
-	}
-	//上边界
-	if($('#oldImage').height() + $('#oldImage').position().top <= 10){
-		$('#oldImage').animate({
-	    	top: (-$('#oldImage').height() + ($('#cuttingImgBox').height()/3)) + 'px'
-	    },100);
+			_self.checkError();
+			setTimeout(function(){
+			    _self.imageInfo.now_width = $('#'+_self.imgID).width();
+				_self.imageInfo.now_height = $('#'+_self.imgID).height();
+				_self.imageInfo.left = $('#'+_self.imgID).position().left;
+			    _self.imageInfo.top = $('#'+_self.imgID).position().top;
+		    },130);
+		    _self.imageInfo.pinchendTime = new Date().getTime();
+		});
+    },
+    setInit:function(){
+    	$('#'+this.cuttingImgBoxID).width($('#'+this.cuttingImgBoxID).width());
+		$('#'+this.cuttingImgBoxID).height($('#'+this.cuttingImgBoxID).height());
+		$('#'+this.imgID).width($('#'+this.imgID).width());
+		$('#'+this.imgID).height($('#'+this.imgID).height());
+    },
+    touch:function(event, _self){
+	    var event = event || window.event;
+	    switch(event.type){
+	        case "touchstart":
+	            _self.imageInfo.tempLeft = $('#'+_self.imgID).position().left;
+	            _self.imageInfo.tempTop = $('#'+_self.imgID).position().top;
+	            break;
+	        case "touchend":
+	        	//双击放大缩小图片
+	        	var touchendTime = new Date().getTime();
+	        	if(touchendTime - _self.imageInfo.pinchendTime > 300){
+	        	//判断双击时间间隔
+		        	if(touchendTime - _self.imageInfo.touchendTime < 300){
+		        		if(_self.imageInfo.zoomState){
+			        		$('#oldImage').css({
+						    	width: $('#'+_self.cuttingImgBoxID).height() * _self.imageInfo.imgAspectRatio,
+						    	height : $('#'+_self.cuttingImgBoxID).height(),
+						    	left: ($('#'+_self.cuttingImgBoxID).width() - ($('#'+_self.cuttingImgBoxID).height() * _self.imageInfo.imgAspectRatio))/2,
+						    	top: 0
+						    });
+						    _self.imageInfo.zoomState = false;
+			        	}else{
+			        		$('#oldImage').css({
+						    	width: _self.imageInfo.width,
+						    	height : _self.imageInfo.height,
+						    	left: 0,
+						    	top: 0
+						    });
+						    _self.imageInfo.zoomState = true;
+			        	}
+			        	
+					    _self.imageInfo.now_width = $('#'+_self.imgID).width();
+						_self.imageInfo.now_height = $('#'+_self.imgID).height();
+						_self.imageInfo.left = $('#'+_self.imgID).position().left;
+					    _self.imageInfo.top = $('#'+_self.imgID).position().top;
+					    
+		        	}
+	        	}
+	        	_self.imageInfo.touchendTime = new Date().getTime();
+
+	        	//给隐藏域赋值
+	        	setTimeout(function(){
+		        	$('#'+_self.cuttingImgBoxID+' > input[name=picW]').val(Math.round($('#'+_self.imgID).width()));
+		        	$('#'+_self.cuttingImgBoxID+' input[name=picH]').val(Math.round($('#'+_self.imgID).height()));
+		        	$('#'+_self.cuttingImgBoxID+' input[name=picX]').val(-Math.round($('#'+_self.imgID).position().left));
+		        	$('#'+_self.cuttingImgBoxID+' input[name=picY]').val(-Math.round($('#'+_self.imgID).position().top));
+		        	$('#'+_self.cuttingImgBoxID+' input[name=picCropW]').val(Math.round($('#'+_self.cuttingImgBoxID).width()));
+		        	$('#'+_self.cuttingImgBoxID+' input[name=picCropH]').val(Math.round($('#'+_self.cuttingImgBoxID).height()));
+	        	},180);
+	            break;
+	    }
+	},
+	checkError:function(){
+		//边界值出界状态
+		var boundaryState = {
+			top: false,
+			right: false,
+			bottom: false,
+			left: false,
+			outNum:0
+		}
+		//检测图片移动边界值
+		//右边界
+		if($('#'+this.imgID).position().left >= $('#'+this.cuttingImgBoxID).width()-10){
+		    boundaryState.right = true;
+		    boundaryState.outNum++;
+		}
+		//下边界
+		if($('#'+this.imgID).position().top >= $('#'+this.cuttingImgBoxID).height()-10){
+		    boundaryState.bottom = true;
+		    boundaryState.outNum++;
+		}
+		//左边界
+		if($('#'+this.imgID).width() + $('#'+this.imgID).position().left <= 10){
+		    boundaryState.left = true;
+		    boundaryState.outNum++;
+		}
+		//上边界
+		if($('#'+this.imgID).height() + $('#'+this.imgID).position().top <= 10){
+		    boundaryState.top = true;
+		    boundaryState.outNum++;
+		}
+
+		//两边出界
+		if(boundaryState.outNum == 2){
+			if(boundaryState.top && boundaryState.right){
+				$('#'+this.imgID).animate({
+			    	top: (-$('#'+this.imgID).height() + ($('#'+this.cuttingImgBoxID).height()/3)) + 'px',
+			    	left: ($('#'+this.cuttingImgBoxID).width() - ($('#'+this.cuttingImgBoxID).width()/3)) + 'px'
+			    },100);
+			}else if(boundaryState.right && boundaryState.bottom){
+				$('#'+this.imgID).animate({
+			    	left: ($('#'+this.cuttingImgBoxID).width() - ($('#'+this.cuttingImgBoxID).width()/3)) + 'px',
+			    	top: ($('#'+this.cuttingImgBoxID).height() - ($('#'+this.cuttingImgBoxID).height()/3)) + 'px'
+			    },100);
+			}else if(boundaryState.bottom && boundaryState.left){
+				$('#'+this.imgID).animate({
+			    	top: ($('#'+this.cuttingImgBoxID).height() - ($('#'+this.cuttingImgBoxID).height()/3)) + 'px',
+			    	left: (-$('#'+this.imgID).width() + ($('#'+this.cuttingImgBoxID).width()/3)) + 'px'
+			    },100);
+			}else if(boundaryState.left && boundaryState.top){
+				$('#'+this.imgID).animate({
+			    	left: (-$('#'+this.imgID).width() + ($('#'+this.cuttingImgBoxID).width()/3)) + 'px',
+			    	top: (-$('#'+this.imgID).height() + ($('#'+this.cuttingImgBoxID).height()/3)) + 'px'
+			    },100);
+			}
+		}else{//单边出界
+			if(boundaryState.top){
+				$('#'+this.imgID).animate({
+			    	top: (-$('#'+this.imgID).height() + ($('#'+this.cuttingImgBoxID).height()/3)) + 'px'
+			    },100);
+			}else if(boundaryState.right){
+				$('#'+this.imgID).animate({
+			    	left: ($('#'+this.cuttingImgBoxID).width() - ($('#'+this.cuttingImgBoxID).width()/3)) + 'px'
+			    },100);
+			}else if(boundaryState.bottom){
+				$('#'+this.imgID).animate({
+			    	top: ($('#'+this.cuttingImgBoxID).height() - ($('#'+this.cuttingImgBoxID).height()/3)) + 'px'
+			    },100);
+			}else if(boundaryState.left){
+				$('#'+this.imgID).animate({
+			    	left: (-$('#'+this.imgID).width() + ($('#'+this.cuttingImgBoxID).width()/3)) + 'px'
+			    },100);
+			}
+		}
+		//恢复状态
+		boundaryState = {
+			top: false,
+			right: false,
+			bottom: false,
+			left: false,
+			outNum:0
+		}
 	}
 }
+var cuttingImg = new AlexCuttingImg({cuttingImgBoxID:'cuttingImgBox', imgID:'oldImage'});
+cuttingImg;
